afe51bd9ce4600f8f65527d62111143675a8eb06
albina1902 <parkkira00@gmail.com>
2024-12-19 15:18:21 +0600

    neon tech

---
+++ db/migrate.ts
@@ -0,0 +1,17 @@
+
+import { migrate } from "drizzle-orm/neon-http/migrator";
+import { db } from "./drizzle";
+
+const main = async () => {
+  try {
+    await migrate(db, {
+      migrationsFolder: "./db/migrations",
+    });
+    console.log("Migration Completed");
+  } catch (error) {
+    console.log("error during migration: ", error);
+    process.exit(1);
+  }
+};
+
+main();
